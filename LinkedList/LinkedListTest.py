2d6ded1b324b977203eaca9b22b1564918e85013
sriv.srivastava@gmail.com <sriv.srivastava@gmail.com>
2018-11-30 22:32:17 +0530

    merge lists

--- LinkedList/LinkedListTest.py
+++ LinkedList/LinkedListTest.py
@@ -4,21 +4,54 @@ class Node:
         self.next = next_node
 
 class LinkedList:
-    def __init__(self):
-        self.head = None
-    def printList(self):
-        tempNode = self.head
-        while tempNode:
-            print(tempNode.data, end = ' ')
-            tempNode = tempNode.next
+    #def __init__(self):
+    #    self.head = None
+    def printList(self, node):
+        while node:
+            print(node.data, end = ' ')
+            node = node.next
         #This statment is used to insert a new line after the result
         print()
 
-if __name__ == '__main__':
+    def mergeLinkedLists(self, node1, node2):
+        head = tail = Node()
+        while node1 and node2:
+            if node1.data < node2.data:
+                tail.next, node1 = node1, node1.next
+            else:
+                tail.next, node2 = node2, node2.next
+            tail = tail.next
+
+        if node1 is None:
+            tail.next = node2
+        else:
+            tail.next = node1
+        return head.next
+
+def testPrintList():
     llist = LinkedList()
-    llist.head = Node(1)
+    first = Node(1)
     second = Node(2)
     third = Node(3)
-    llist.head.next = second
+    first.next = second
     second.next = third
-    llist.printList()
+    llist.printList(first)
+
+def testMergeLists():
+    llist = LinkedList()
+    first1 = Node(1)
+    second1 = Node(4)
+    third1 = Node(7)
+    first1.next = second1
+    second1.next = third1
+
+    first2 = Node(2)
+    second2 = Node(3)
+    third2 = Node(9)
+    first2.next = second2
+    second2.next = third2
+
+    result = llist.mergeLinkedLists(first1, first2)
+    llist.printList(result)
+if __name__ == '__main__':
+    testMergeLists()
