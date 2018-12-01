dc128c3278f31694a3614c8297cf2738a5471f75
sriv.srivastava@gmail.com <sriv.srivastava@gmail.com>
2018-12-01 18:07:24 +0530

    another lnked list question

--- LinkedList/LinkedListTest.py
+++ LinkedList/LinkedListTest.py
@@ -44,24 +44,48 @@ class LinkedList:
         if slow == None:
             return None
         fast = node.next.next
-        while slow != None and fast != None and fast.next != None:
-            if slow == fast:
+        while slow and fast  and fast.next:
+            if slow is fast:
                 #loop exists
                 return self.cycleLength(slow)
             else:
                 slow = slow.next
                 fast = fast.next.next
         return None
+    def length(self, node):
+        count = 0
+        while node:
+            count += 1
+            node = node.next
+        return count
+    def overlapping_nocycle_lists(self, node1, node2):
+        len1, len2 = self.length(node1), self.length(node2)
+        if len1 > len2:
+            #make node2 longer one always. it will simplify the code
+            node2, node1 = node2, node1
+        for _ in range(abs(len1 - len2)):
+            node2 = node2.next
+        while node1 and node2 and node1 is not node2:
+            node1, node2 = node1.next, node2.next
+        return node1
 
     def cycleLength(self, node):
         tmpNode = node.next
         step = 1
-        while tmpNode != node:
+        while tmpNode is not node:
             step += 1
             tmpNode = tmpNode.next
 
         return step
 
+def test_length():
+    llist = LinkedList()
+    first = Node(1)
+    second = Node(2)
+    third = Node(3)
+    first.next = second
+    second.next = third
+    print(llist.length(first))
 
 def testPrintList():
     llist = LinkedList()
@@ -97,6 +121,20 @@ def testCycle():
     second.next = third
     third.next = first
     print(llist.detectCycle2(first))
+def test_overlapping_nocycle_lists():
+    llist = LinkedList()
+    first1 = Node(1)
+    second1 = Node(4)
+    third1 = Node(8)
+    first1.next = second1
+    second1.next = third1
 
+    first2 = Node(2)
+    second2 = Node(3)
+
+    first2.next = second2
+    second2.next = third1
+    start_common_node = llist.overlapping_nocycle_lists(first1, first2)
+    llist.printList(start_common_node)
 if __name__ == '__main__':
-    testCycle()
+    test_overlapping_nocycle_lists()
